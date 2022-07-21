5835aa547fafcf09c2382e818c03470746e76218
Seraphina <queenaseraphina@gmail.com>
2022-07-21 17:55:20 +0200

    Comment out teh unused gpu blacklisting option

--- configuration.nix
+++ configuration.nix
@@ -21,7 +21,7 @@
     kernelModules = [
       "vfio_virqfd" "vfio_pci" "vfio_iommu_type1" "vfio"
     ];
-    extraModprobeConfig = "options vfio-pci ids=10de:1c82,10de:0fb9";
+    #extraModprobeConfig = "options vfio-pci ids=10de:1c82,10de:0fb9";
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
